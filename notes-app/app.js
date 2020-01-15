a71db4b9e584d0c98025a08e33f8cb02b765d856
Mohamed Fouad <mohamed.m.fouad.mahmoud@gmail.com>
2020-01-15 15:36:50 +0200

    adding color foe cli

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,14 @@
 const validator = require('validator');
-
+const chalk = require('chalk');
+const log = console.log;
 const getNotes = require('./utils');
 
 console.log(getNotes());
 
 
 console.log(validator.isEmail('Mohamed@example.com'))
+log(chalk.keyword('orange')('Yay for orange colored text!'));
+ 
+
+
+console.log(process.argv);
